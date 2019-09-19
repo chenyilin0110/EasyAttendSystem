bb0838d20cbc5609a912134dee8fc5dfc58e1ae0
yilin <a0989735018@gmail.com>
2019-09-19 18:08:57 +0800

    revise fail that only one student absencetee

--- EasyAttendSystem_python/countEachStudentNoAttendFrequency.py
+++ EasyAttendSystem_python/countEachStudentNoAttendFrequency.py
@@ -4,22 +4,26 @@ import operator
 allNoAttendList = np.loadtxt('result/allNoAttendList.txt', dtype=np.str, delimiter='\n')
 
 allStudent = []
+count = len(np.atleast_1d(allNoAttendList))
 
-for i in range(len(np.atleast_1d(allNoAttendList))):
-    flag = 0
-    if len(allStudent) == 0:
-        allStudent.append([])
-        allStudent[0].append(allNoAttendList[0])
-        allStudent[0].append(1)
-    else:
-        for j in range(len(allStudent)):
-            if operator.eq(allNoAttendList[i], allStudent[j][0]):
-                allStudent[j][1] += 1
-                flag = 1
-        if flag == 0:
+if count == 1:
+    print(allNoAttendList)
+else:
+    for i in range(len(np.atleast_1d(allNoAttendList))):
+        flag = 0
+        if len(allStudent) == 0:
             allStudent.append([])
-            allStudent[len(allStudent)-1].append(allNoAttendList[i])
-            allStudent[len(allStudent)-1].append(1)
+            allStudent[0].append(allNoAttendList[0])
+            allStudent[0].append(1)
+        else:
+            for j in range(len(allStudent)):
+                if operator.eq(allNoAttendList[i], allStudent[j][0]):
+                    allStudent[j][1] += 1
+                    flag = 1
+            if flag == 0:
+                allStudent.append([])
+                allStudent[len(allStudent)-1].append(allNoAttendList[i])
+                allStudent[len(allStudent)-1].append(1)
 
 for i in range(len(allStudent)):
     print(allStudent[i][0],allStudent[i][1])
